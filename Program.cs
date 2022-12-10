ec8c329ddbf37beb8dcf80a0e79a6e880920d66a
Hasan Huseynli <hasan@Hasans-MacBook-Pro.local>
2022-12-10 23:09:51 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Huseynli;
+class Program
+{
+    static void Main()
+    {
+        int eded = 600;
+        int faiz = 90;
+
+        int ededinFaizi = eded * faiz / 100;
+
+        Console.WriteLine(ededinFaizi);
+
+        Console.ReadKey();
+    }
+}
+
